7c784c4b5b2174a09c42da91d08d729e493a44f5
Timo Turtiainen <timo.turtiainen@icloud.com>
2024-02-16 18:15:03 +0200

    2c

--- osa1/puhelinluettelo/src/App.jsx
+++ osa1/puhelinluettelo/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FilterPersons from "./components/FilterPersons";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  // powershell -ExecutionPolicy Bypass -Command "json-server --port=3001 --watch db.json"
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchText, setSearchText] = useState("");
