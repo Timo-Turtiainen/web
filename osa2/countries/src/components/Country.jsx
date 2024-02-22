70c95afaafb9dcfef73fa16b6709c51d1b1aa536
Timo-Turtiainen <timo.turtiainen@icloud.com>
2024-02-22 10:20:57 +0200

    part 2.19

--- osa2/countries/src/components/Country.jsx
+++ osa2/countries/src/components/Country.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Country({ country }) {
-  console.log(country);
+function Country({ filteredData }) {
+  console.log(filteredData);
+  let list = [];
+  for (const item in filteredData.languages) {
+    list.push(item);
+  }
+
   return (
     <div>
       {/* name */}
-
+      <h1>{filteredData.name.common}</h1>
       {/* capital city */}
-      {/* population */}
+      <h2>Capital {filteredData.capital}</h2>
       {/* area */}
-
+      <h4>Area: {filteredData.area}</h4>
+      {/* population */}
+      <h4>Population: {filteredData.population}</h4>
       {/* h4 languages */}
-      {/* ul  */}
-      {/* li  */}
+      <h3>languages:</h3>
+      {/* List of languages */}
+      <ul>
+        {list.map((item, index) => {
+          return <li key={index}>{filteredData.languages[item]}</li>;
+        })}
+      </ul>
+      <img src={filteredData.flags.png} />
     </div>
   );
 }
--- osa2/countries/src/components/CountryList.jsx
+++ osa2/countries/src/components/CountryList.jsx
@@ -4,6 +4,7 @@ import Country from "./Country";
 function CountryList({ countries, input }) {
   let count = 0;
 
+  // Filter method checks that input and country name matches
   const filteredData = countries.filter((item) => {
     if (item.name.common.toLowerCase().includes(input.toLowerCase())) {
       count++;
@@ -13,7 +14,10 @@ function CountryList({ countries, input }) {
   console.log(filteredData.length);
   return (
     <>
-      {filteredData.length <= 10 ? (
+      {/* Show component if one match */}
+      {filteredData.length === 1 ? (
+        <Country filteredData={filteredData[0]} />
+      ) : filteredData.length <= 10 ? (
         <div>
           {filteredData.map((item) => {
             return <h2 key={item.name.official}>{item.name.common}</h2>;
