0671beb36c1998d70a0b240ca202a1bf92d5d170
Timo-Turtiainen <timo.turtiainen@icloud.com>
2024-02-27 13:53:13 +0200

    test

--- osa3/index.js
+++ osa3/index.js
@@ -31,55 +31,55 @@ const PORT = process.env.PORT;
 //   return maxId + 1;
 // };
 
-/* GET all persons */
-app.get("/api/persons", (request, response) => {
-  // console.log("Server GET All", data);
+// /* GET all persons */
+// app.get("/api/persons", (request, response) => {
+//   // console.log("Server GET All", data);
 
-  Person.find({}).then((persons) => {
-    response.json(persons);
-    console.log("backend get all method ", persons);
-  });
+//   Person.find({}).then((persons) => {
+//     response.json(persons);
+//     console.log("backend get all method ", persons);
+//   });
 
-  mongoose.connection.close();
-});
+//   mongoose.connection.close();
+// });
 
-/* 3.2 backend step 2 */
-app.get("/info", (request, response) => {
-  const dataLength = data.persons.length;
-  const dateTime = new Date();
-  const text = `Phonebook has info for ${dataLength} \npeople ${dateTime}`;
-  response.send(text);
-});
+// /* 3.2 backend step 2 */
+// app.get("/info", (request, response) => {
+//   const dataLength = data.persons.length;
+//   const dateTime = new Date();
+//   const text = `Phonebook has info for ${dataLength} \npeople ${dateTime}`;
+//   response.send(text);
+// });
 
-/* GET person by id */
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+// /* GET person by id */
+// app.get("/api/persons/:id", (request, response) => {
+//   Person.findById(request.params.id).then((person) => {
+//     response.json(person);
+//   });
 
-  // const id = Number(request.params.id);
-  // // console.log("Requested id:", id);
-  // // console.log("Dataset:", data); // Log the entire data object to see its structure
-  // let person = data.persons.find((person) => Number(person.id) === id);
-  // // console.log("Found person:", person); // Log the person found by the id
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   console.log("Person not found");
-  //   response.status(404).end();
-  // }
-  mongoose.connection.close();
-});
+//   // const id = Number(request.params.id);
+//   // // console.log("Requested id:", id);
+//   // // console.log("Dataset:", data); // Log the entire data object to see its structure
+//   // let person = data.persons.find((person) => Number(person.id) === id);
+//   // // console.log("Found person:", person); // Log the person found by the id
+//   // if (person) {
+//   //   response.json(person);
+//   // } else {
+//   //   console.log("Person not found");
+//   //   response.status(404).end();
+//   // }
+//   mongoose.connection.close();
+// });
 
-/* DELETE person */
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  // console.log("id to be deleted:", id);
-  // console.log("Dataset:", data); // Log the entire data object to see its structure
-  data.persons = data.persons.filter((person) => Number(person.id) !== id);
-  // console.log("server delete Dataset:", data); // After delete
-  response.status(204).end;
-});
+// /* DELETE person */
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   // console.log("id to be deleted:", id);
+//   // console.log("Dataset:", data); // Log the entire data object to see its structure
+//   data.persons = data.persons.filter((person) => Number(person.id) !== id);
+//   // console.log("server delete Dataset:", data); // After delete
+//   response.status(204).end;
+// });
 
 /* 3.6: puhelinluettelon backend step6
 Tee uuden numeron lisäykseen virheiden käsittely. Pyyntö ei saa onnistua, jos
@@ -87,7 +87,7 @@ Tee uuden numeron lisäykseen virheiden käsittely. Pyyntö ei saa onnistua, jos
  - lisättävä nimi on jo luettelossa */
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-
+  console.log("body content", body.content);
   /* There is no content */
   if (body.content === undefined) {
     return response.status(400).json({ error: "content missing" });
